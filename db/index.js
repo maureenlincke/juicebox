30f6b263c0e14f7daefb6afb1ddb94a0d14fe87e
Maureen <mlincke15@gmail.com>
2021-10-21 21:28:13 -0600

    changes

--- db/index.js
+++ db/index.js
@@ -5,3 +5,17 @@ const client = new Client('postgres://localhost:5432/juicebox-dev');
 module.exports = {
     client,
 }
+
+async function getAllUsers() {
+    const { rows } = await client.query(
+      `SELECT id, username 
+      FROM users;
+    `);
+  
+    return rows;
+  }
+  
+  module.exports = {
+    client,
+    getAllUsers,
+  }
