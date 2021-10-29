71f2703c2cd58b843a189a753a30cf24d09582c1
Maureen <mlincke15@gmail.com>
2021-10-29 08:28:46 -0600

    part two

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const PORT = 3000;
 const express = require('express');
 const server = express();
@@ -10,6 +12,8 @@ server.use(express.json())
 
 const apiRouter = require('./api');
 server.use('/api', apiRouter);
+
+
 
 server.listen(PORT, () => {
   console.log('The server is up on port', PORT)
